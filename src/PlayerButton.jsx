a72a29f6675965bce29d67d6068e73418eae80e1
Jon-Hudak <shigshagor@gmail.com>
2022-09-07 00:05:42 -0400

    wip: Changed from px to rem

--- src/PlayerButton.jsx
+++ src/PlayerButton.jsx
@@ -1,15 +1,16 @@
-import styled from "styled-components";
+ import styled from "styled-components";
 
 export const PlayerButton = styled.button`
 
 color: #ffffff;
-padding: 0.7em 1em;
-width:150px;
-margin: 0.5em;
-font-size: 18px;
-/* border-radius: 0.2em; */
+padding: 0.7rem 1rem;
+height:6rem;
+width:16rem;
+margin: 0.5rem;
+font-size: 2.0rem;
+/* border-radius: 0.2rem; */
 background: #4e4b4b;
-
+flex-shrink:1;
 border-color: orange;
 /* box-shadow: 1px px 3px #5c5c5c,
             -1px -1px 3px #636363; */
@@ -20,24 +21,28 @@ box-shadow: inset 1px 1px 7px #5c5c5c,
 }
 @media (max-height:500px){
     
-    font-size: 20px;
-    height:60px;
+    font-size: 1.5rem;
+    height:6.0rem;
     flex-grow:1;
+
+    max-width:11rem;
 }
-@media (max-width:1130px){
+/* @media (max-width:1130px){
     
     
-    /* font-size:12px; */
+    font-size:12px;
     
-}
-@media (min-height:501px){
-    @media (max-width:980px){
+} */
+@media (min-height: 500px){
+    @media (max-width:910px){
         
         
-        font-size:25px;
-        height:100px;
-        width:90%;
+        //font-size:1.5rem;
+        height:5.8rem;
+        width:20rem;
     }
+    
 }
+
 `
 
